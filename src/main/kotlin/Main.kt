a321cb73af41fe89102fd980778ff329cb60aba1
PruthiviRaj27 <pruthiviraj@testpress.in>
2023-05-27 19:40:15 +0530

    temp

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,6 +6,6 @@ private val date get() = Calendar.getInstance().time.toString()
 fun main() {
     println("Hello World!")
     println("Start $date")
-    Thread.sleep(15000).
+    Thread.sleep(15000)
     println("End $date")
 }
